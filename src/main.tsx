bb584666c8f4d89819f6077c98b110109f3411c3
eshginfarzali <git>
2023-11-07 16:16:54 +0400

    feat: husky and github workflows

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { router } from './router';
+import { router} from './router';
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
